7affb1d41044a12a51f959f3cc50404046d1fca3
Travis Mosley <travis@travismosley.com>
2013-07-06 04:00:57 +0000

    updated web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var msgBuf = fs.readFileSync('./index.html');
+  response.send(msgBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
